3f7e0f677cc17bcae1d9470dcf0847322ef44bf1
takkanm <takkanm@gmail.com>
2017-01-06 18:08:01 +0900

    add init subcommand

--- main.go
+++ main.go
@@ -22,10 +22,28 @@ func (c *ListStars) Run(args []string) int {
 	return 0
 }
 
+type AddToken struct {
+}
+
+func (c *AddToken) Synopsis() string {
+	return "add GitHub token"
+}
+
+func (c *AddToken) Help() string {
+	return "Usage: gstar init github_token"
+}
+
+func (c *AddToken) Run(args []string) int {
+	return 0
+}
+
 func main() {
 	c := cli.NewCLI("gstar", "0.1.0")
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
+		"init": func() (cli.Command, error) {
+			return &AddToken{}, nil
+		},
 		"list": func() (cli.Command, error) {
 			return &ListStars{}, nil
 		},
