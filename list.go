73eeba8982eb12f89161c26467c7457dce71bdaa
takkanm <takkanm@gmail.com>
2017-01-08 23:01:07 +0900

    extract files

--- list.go
+++ list.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
-	"strconv"
 )
 
 type ListStars struct {
-}
-
-type StarList struct {
-	page  int
-	stars []Star
-}
-
-type Star struct {
-	FullName    string `json:"full_name"`
-	Description string `json:"description"`
-	Language    string `json:language`
 }
 
 func (c *ListStars) Synopsis() string {
@@ -66,25 +52,4 @@ func (c *ListStars) Run(args []string) int {
 	}
 
 	return 0
-}
-
-func RequestStars(token string, page int, ch chan<- StarList) {
-	starList := StarList{page: page}
-	starJSON := GetStarList(string(token), page)
-
-	json.Unmarshal([]byte(starJSON), &starList.stars)
-	ch <- starList
-}
-
-func GetStarList(token string, page int) string {
-	client := &http.Client{}
-
-	req, _ := http.NewRequest("GET", "https://api.github.com/user/starred?page="+strconv.Itoa(page), nil)
-	req.Header.Add("Authorization", "token "+token)
-
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	return string(body)
 }
---
+++ request.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+type StarList struct {
+	page  int
+	stars []Star
+}
+
+type Star struct {
+	FullName    string `json:"full_name"`
+	Description string `json:"description"`
+	Language    string `json:language`
+}
+
+func RequestStars(token string, page int, ch chan<- StarList) {
+	starList := StarList{page: page}
+	starJSON := GetStarList(string(token), page)
+
+	json.Unmarshal([]byte(starJSON), &starList.stars)
+	ch <- starList
+}
+
+func GetStarList(token string, page int) string {
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", "https://api.github.com/user/starred?page="+strconv.Itoa(page), nil)
+	req.Header.Add("Authorization", "token "+token)
+
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	return string(body)
+}
